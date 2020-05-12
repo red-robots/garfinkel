1cec3433bab0cb0f90835ff0676d81982d564a95
Lisa <lisaqdebona@gmail.com>
2020-05-12 00:25:25 -0600

    Styling Careers page

---
+++ page-careers.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Template Name: Careers
+ */
+
+get_header(); 
+$header_image = get_field("hero_image"); 
+$has_header_image = ($header_image) ? 'has-header-image':'no-header-image';
+global $post;
+$post_id = $post->ID;
+$slug = $post->post_name;
+$taxonomy = 'category';
+$post_type = 'post';
+$currentPage = get_permalink($post_id);
+?>
+
+<div id="primary" class="content-area careerspage default cf <?php echo $has_header_image ?>">
+	<main id="main" class="site-main cf wrapper" role="main">
+		
+		
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php
+			$bottombox = get_field("bottombox",$post_id);
+			$benefits = get_field("benefits",$post_id);
+			$mainText = strip_tags(get_the_content($post_id));
+			$mainText = preg_replace('/\s+/', '', $mainText);
+			if (strpos($mainText, 'string(0)') !== false) {
+			    $mainText = '';
+			}
+			$colClass = ($benefits) ? 'twocol':'onecol';
+		?>
+
+		<div class="top-content-wrap <?php echo $colClass ?>">
+		
+			<?php if (empty($header_image)) { ?>
+			<header class="entry-header fw">
+				<div class="wrapper"><h1 class="page-title"><?php echo get_the_title($post_id); ?></h1></div>
+			</header>
+			<?php } ?>
+			
+			<div id="career-left" class="leftcol">
+				
+				<?php if ( $mainText ) { ?>
+				<div class="entry-content cf">
+					<?php the_content(); ?>
+				</div>
+				<?php } ?>
+				
+				<?php get_template_part("parts/content","careers"); ?>
+			</div>
+			
+			<div id="career-right" class="rightcol">
+				<?php if ( $benefits ) { ?>
+				<div class="benefits">
+					<?php echo $benefits; ?>
+				</div>
+				<?php } ?>
+			</div>
+
+
+			<?php if ($bottombox) { ?>
+			<div class="bottomBlueBox">
+				<div class="wrap"><?php echo $bottombox ?></div>
+			</div>	
+			<?php } ?>
+
+		</div>
+		<?php endwhile; ?>
+
+		<div id="mobile-sidebar" class="fw"></div>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<script>
+jQuery(document).ready(function($){
+	$(".jobtitle").on("click",function(){
+		var parent = $(this).parents(".career-info");
+		parent.toggleClass('open');
+		parent.find(".jobdescription").slideToggle();
+	});
+	// var screenWidth = $(window).width();
+	// if( screenWidth < 800 ) {
+	// 	$("#career-right .benefits").appendTo('#mobile-sidebar');
+	// } else {
+
+	// }
+});
+</script>
+<?php
+get_footer();
---
+++ parts/content-careers.php
@@ -0,0 +1,58 @@
+<?php
+$posts_per_page = -1;
+$post_type = 'careers';
+$paged = ( get_query_var( 'pg' ) ) ? absint( get_query_var( 'pg' ) ) : 1;
+$args = array(
+	'posts_per_page'=> $posts_per_page,
+	'post_type'		=> $post_type,
+	'post_status'	=> 'publish'
+);
+$allCareers = get_posts($args);
+$exclude = array();
+$careersList = array();
+if($allCareers) {
+	foreach($allCareers as $a) {
+		$id = $a->ID;
+		$responsibilities = get_field("responsibilities",$id);
+		$requirements = get_field("requirements",$id); 
+		$has_content = ($responsibilities || $requirements) ? true:false;
+		if($has_content) {
+			$careersList[] = $a;
+		} else {
+			$exclude[] = $id;
+		}
+	}
+}
+if($exclude) {
+	$args['post__not_in'] = $exclude;
+}
+$careers = new WP_Query($args);
+if ( $careers->have_posts() ) {  ?>
+<section class="section-careers fw">
+	<div class="section-title">Available Positions</div>
+	<?php while ( $careers->have_posts() ) : $careers->the_post();  ?>
+		<?php 
+		$responsibilities = get_field("responsibilities");
+		$requirements = get_field("requirements"); 
+		?>
+		<div class="career-info">
+			<h2 class="jobtitle"><?php echo get_the_title(); ?> <span class="arrow"></span></h2>
+			<div class="jobdescription">
+				<?php if ($responsibilities) { ?>
+				<div class="jobinfo responsibilities">
+					<h2 class="sub">Responsibilities</h2>
+					<div class="wrap"><?php echo $responsibilities ?></div>
+				</div>	
+				<?php } ?>
+
+				<?php if ($requirements) { ?>
+				<div class="jobinfo requirements">
+					<h2 class="sub">Requirements</h2>
+					<div class="wrap"><?php echo $requirements ?></div>
+				</div>	
+				<?php } ?>
+			</div>
+		</div>
+	<?php endwhile; wp_reset_postdata(); ?>
+</section>
+<?php } ?>
